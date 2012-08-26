4e901b00e81aa9369f84f5d0cb073d8cb562ddba
Sho Hashimoto <hashimoto@shokai.org>
2012-08-26 17:04:54 +0900

    play palapala on web

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,26 @@
+var play = function(){
+  display(0);
+};
+
+var display = function(i){
+  if(!i) i = 0;
+  var imgs = $('#imgs img');
+  if(i > imgs.length - 1) i = 0;
+  if(imgs.length > 0) $('.main img').attr('src', imgs[i].src);
+  setTimeout(function(){
+    display(i+1);
+  }, 200);
+};
+
 $(function(){
-  console.log('start');
   $('#camera').change(function(e){
     var reader = new FileReader();
     reader.onload = function(e){
-      var img = $('<img>').attr('src', reader.result);
-      $('#imgs').append(img);
+      var img = $('<img>').attr('src', reader.result).attr('width', 160).attr('height', 120);
+      $('#imgs').prepend(img);
     };
     reader.readAsDataURL(e.currentTarget.files[0]);
   });
+
+  play();
 });
